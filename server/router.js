71e06deaa3d01038a0505204f625ebef8081e1d3
pingyi Luo <pingyiluo@gmail.com>
2018-03-17 22:39:41 +0800

    u

--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ var Router = require('koa-router');
 
 var router = new Router();
 
-router.get('/', (ctx, next) => {
+router.get('/demo', (ctx, next) => {
     // ctx.router available
     ctx.body = {
         name:'主枢纽'

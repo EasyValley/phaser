6bbdaf6860cca165288ea641f8c1a60cf1d258f4
pingyi Luo <pingyiluo@gmail.com>
2018-03-18 00:43:59 +0800

    u

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 const game = require('./game');
 const boot = require('./state/boot');
+const demo = require('./state/demo');
 boot();
-game.state.start('boot');
+demo();
+
+let arr = ['boot', 'demo'];
+
+run(arr);
+
+
+
+function run(arr) {
+    game.state.start(arr[0]);
+    arr.reverse();
+    setTimeout(() => {
+        run(arr);
+    }, 100);
+}
--- src/state/boot.js
+++ src/state/boot.js
@@ -4,7 +4,10 @@ const game = require('../game');
 function boot() {
     game.state.add('boot', function () {
         this.preload = function () {
-            console.log('wwooo ss');
+            game.load.image('thorn', require('../img/thorn_lazur.png'));
+        }
+        this.create = function () {
+            game.add.image(0, 0, 'thorn');
         }
     });
 }

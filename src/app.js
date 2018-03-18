958f6a1213eecc46e3d253a50e2aa260d5961be0
pingyi Luo <pingyiluo@gmail.com>
2018-03-18 10:26:31 +0800

    update

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@ const game = require('./game');
 const boot = require('./state/boot');
 const demo = require('./state/demo');
 boot();
-demo();
+// demo();
+game.state.start('boot');
+// let arr = ['boot', 'demo'];
 
-let arr = ['boot', 'demo'];
-
-run(arr);
+// run(arr);
 
 
 
-function run(arr) {
-    game.state.start(arr[0]);
-    arr.reverse();
-    setTimeout(() => {
-        run(arr);
-    }, 1000);
-}
+// function run(arr) {
+//     game.state.start(arr[0]);
+//     arr.reverse();
+//     setTimeout(() => {
+//         run(arr);
+//     }, 1000);
+// }
--- src/state/boot.js
+++ src/state/boot.js
@@ -5,10 +5,25 @@ function boot() {
     game.state.add('boot', function () {
         this.preload = function () {
             game.load.image('thorn', require('../img/thorn_lazur.png'));
+            game.load.spritesheet('mummy', require('../img/mummy37x45.png'), 37, 45, 18);
         }
+        var mg;
         this.create = function () {
-            game.add.image(0, 0, 'thorn');
+            mg = game.add.image(0, 0, 'thorn');
+            let mummy = game.add.sprite(200, 260, 'mummy', 5);
+            mummy.inputEnabled = true;
+            mummy.input.enableDrag();
+            console.log(mummy);
+            mummy.scale.set(4);
+            let anim = mummy.animations.add('walk');
+            anim.play(10, true);
+            console.log('hhh')
         }
+
+        this.update = function () {
+            mg.x -= 1;
+        }
+
     });
 }
 
